08756dcac52b882955d690055d203b4dc11a2845
khanans786 <120304835+khanans786@users.noreply.github.com>
2023-01-03 02:06:33 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+create or replace table employees(
+States                                 varchar(100)
+,Number_of_Employees                   varchar(100)
+,Number_of_Employees_in_Pune           varchar(100)
+,Number_of_Employees_in_Mumbai         varchar(100)
+,Number_of_Employees_in_Hyderabad      varchar(100)
+,Number_of_Employees_in_Banglore       varchar(100)
+);
+
